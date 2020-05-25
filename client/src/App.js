bef9bee2a2eb5b227fa4bf1fb59bc4f229742d88
Arturo <iglesias294@icloud.com>
2020-05-25 18:33:41 -0400

    Added settings page, stripe component
    settings page with tabs panel,
    stripe component working test with card 4242 4242 4242 4242 any date any CVV

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Login from "./pages/Login";
 import Contest from "./pages/Contest";
 import Profile from "./pages/Profile";
 import Submission from "./pages/SubmitSubmission";
+import Settings from './pages/Settings'
 
 import { AuthContext } from "./components/UserContext";
 
@@ -28,13 +29,14 @@ function App() {
     <MuiThemeProvider theme={theme}>
       <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
         <BrowserRouter>
-          <Route path="/" component={Header} />
-          <Route exact path="/" render={(props) => <Redirect to="/login" />} />
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/contest" component={Contest} />
-          <Route path="/submission" component={Submission} />
+          <Route path='/' component={Header} />
+          <Route exact path='/' render={(props) => <Redirect to='/login' />} />
+          <Route path='/register' component={Register} />
+          <Route path='/login' component={Login} />
+          <Route path='/profile' component={Profile} />
+          <Route path='/contest' component={Contest} />
+          <Route path='/submission' component={Submission} />
+          <Route path='/settings' component={Settings} />
         </BrowserRouter>
       </AuthContext.Provider>
     </MuiThemeProvider>
