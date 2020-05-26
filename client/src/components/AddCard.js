125ec5aac619cd3c3f71084cebd3cb17ebdfb2dc
Arturo <iglesias294@icloud.com>
2020-05-26 14:07:11 -0400

    Responsive styling for tabs
    Settings page tabs are vertical on large screens and horizontal on mobile

--- client/src/components/AddCard.js
+++ client/src/components/AddCard.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { Button, Typography, Snackbar } from '@material-ui/core'
+import {
+    Button,
+    Typography,
+    Snackbar,
+} from '@material-ui/core'
 import Alert from '@material-ui/lab/Alert'
 import { loadStripe } from '@stripe/stripe-js'
 import {
@@ -34,6 +38,7 @@ function CheckoutForm() {
 
         }
         if (paymentMethod) {
+            console.log(paymentMethod)
             handleAlert(`Your ${paymentMethod?.card?.brand} card ending in ${paymentMethod?.card?.last4} was added successfully`, "success")
         }
     }
@@ -43,94 +48,119 @@ function CheckoutForm() {
         setSeverity(severity)
         setAlertMessage(message)
     }
-    const options = {
-        style: {
-            base: {
-                fontSize: '16px',
-                color: '#252525',
-                '::placeholder': {
-                    color: '#252525',
-                },
-            },
-            invalid: {
-                color: '#9e2146',
-            },
-        },
-    }
-    return (<div>
-        <Snackbar
-            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-            open={openAlert}
-            autoHideDuration={5000}
-            onClose={() => setOpenAlert(false)}
-        >
-            <Alert
-                severity={severity}
+    return (
+        <div>
+            <Snackbar
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                open={openAlert}
+                autoHideDuration={5000}
                 onClose={() => setOpenAlert(false)}
             >
-                {alertMessage}
-            </Alert>
-        </Snackbar>
-        <Typography variant="h3">Payment Details</Typography>
-        <br />
-        <Typography
-            variant="subtitle1"
-        >Enter your card details</Typography>
-        <br />
-        <form
-            style={{ display: 'flex', flexDirection: 'column', flex: 1, width: '100%' }}
-            onSubmit={handleSubmit}>
-
+                <Alert
+                    severity={severity}
+                    onClose={() => setOpenAlert(false)}
+                >
+                    {alertMessage}
+                </Alert>
+            </Snackbar>
+            <Typography variant="h3">Payment Details</Typography>
 
             <Typography
-                className={classes.fieldLabel}
-                variant="caption">Card Number</Typography>
-            <CardNumberElement
-                options={options} />
-            <div style={{ display: 'flex', width: '100%', paddingBottom: 25, paddingTop: 25, }}>
-                <div className={classes.field}>
-                    <Typography
-                        className={classes.fieldLabel}
-                        variant="caption">Card Expiry</Typography>
-                    <CardExpiryElement
-                        options={options}
-                    />
-                </div>
-                <div className={classes.field}>
-                    <Typography
-                        className={classes.fieldLabel}
-                        variant="caption">CVC</Typography>
-                    <CardCvcElement
-                        options={options}
-                    />
-                </div>
-            </div>
+                className={classes.instructionText}
+                variant="subtitle1"
+            >Enter your card details:</Typography>
 
-            <Button
-                variant="outlined"
-                size="large"
-                type="submit"
-                disabled={!stripe}
-                style={{ margin: 'auto', height: 50, width: 150, borderRadius: 0, borderColor: '#252525' }}>
-                Add Card
+            <form
+                onSubmit={handleSubmit}>
+
+                <div style={{ marginLeft: '1rem', paddingTop: '1rem' }}>
+                    <Typography
+                        className={classes.fieldLabel}
+                        variant="caption">Card Number</Typography>
+
+                    <CardNumberElement
+                        options={options} />
+                </div>
+                <div className={classes.inlineFieldContainer}>
+                    <div className={classes.field}>
+                        <Typography
+                            className={classes.fieldLabel}
+                            variant="caption">Card Expiry</Typography>
+                        <CardExpiryElement
+                            options={options}
+                        />
+                    </div>
+                    <div className={classes.field}>
+                        <Typography
+                            className={classes.fieldLabel}
+                            variant="caption">CVC</Typography>
+                        <CardCvcElement
+                            options={options}
+                        />
+                    </div>
+                </div>
+
+                <Button
+                    variant="outlined"
+                    size="large"
+                    type="submit"
+                    disabled={!stripe}
+                    className={classes.button}>
+                    Add Card
             </Button>
-        </form>
-    </div>)
+            </form>
+        </div>)
 }
 
 const stripePromise = loadStripe(stripeKey)
 
 export default function AddPaymentMethod() {
-    return (<Elements stripe={stripePromise}>
-        <CheckoutForm />
-    </Elements>)
+    return (
+        <Elements stripe={stripePromise}>
+            <CheckoutForm />
+        </Elements>
+    )
+}
+//Stripe element style settings
+const options = {
+    style: {
+        base: {
+            fontSize: '16px',
+            color: '#252525',
+            '::placeholder': {
+                color: '#252525',
+            },
+        },
+        invalid: {
+            color: '#9e2146',
+        },
+    },
 }
 
 const useStyles = makeStyles({
+    instructionText: {
+        marginTop: '1rem',
+        marginBottom: '1rem'
+    },
+    inlineFieldContainer: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '100%',
+        marginLeft: '1rem',
+        paddingBottom: '1rem',
+        paddingTop: '1rem',
+    },
     fieldLabel: {
         color: 'gray'
     },
     field: {
         width: '75px'
+    },
+    button: {
+        marginTop: '2rem',
+        height: 50,
+        width: '100%',
+        borderRadius: 0,
+        borderColor: '#252525'
     }
 })
